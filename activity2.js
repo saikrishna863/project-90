7725ea25a7b00dc6ed9c22f811d9bc9e56ba65fc
saikrishna863 <82523294+saikrishna863@users.noreply.github.com>
2021-06-25 15:53:22 +0530

    Add files via upload

---
+++ activity2.js
@@ -0,0 +1,7 @@
+function back() {
+window.Location = "activity_1.html";
+}
+function  getScore()
+{
+score = localStorage.getItem("update").innerHTML = "<h1>Score: " +  score + "</h1>";
+}
---
+++ game_login.js
@@ -0,0 +1,6 @@
+function addUser () {
+player1_name = document.getElementById("player1_name_input").value;
+player1_name = document.getElementById("player1_name_input").value;
+
+window.location("activity_1.html");
+}
